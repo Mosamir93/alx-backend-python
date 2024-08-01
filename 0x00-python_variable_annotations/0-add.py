c1d49ef3b384b68dbcf0c1565de2d96295975c8d
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-08-01 18:55:23 +0300

    task-1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,8 @@
 """
 This module contains a function that adds two floats.
 """
+
+
 def add(a: float, b: float) -> float:
     """Adds two floats and return the result."""
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module concatenates two strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and return the new string.
+    """
+    return str1 + str2
