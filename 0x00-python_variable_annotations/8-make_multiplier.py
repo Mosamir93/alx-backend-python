84ad6ca226ad2f4c73a14b86b7ef7b6956dc4cc6
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-08-01 19:28:53 +0300

    task-8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module takes a float as argument and returns a
+function that multiplies a float by the argument.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    A type-annotated function make_multiplier that takes a float multiplier as
+    argument and returns a function that multiplies a float by multiplier.
+    """
+    def multiplier_function(n: float) -> float:
+        return n * multiplier
+    
+    return multiplier_function
