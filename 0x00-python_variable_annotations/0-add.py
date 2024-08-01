a34d4d6102d2b17932bc0013db86a8429dd60727
mohamed samir <mohamed_elshirbiny@hotmail.com>
2024-08-01 18:51:44 +0300

    readme file and task-0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that adds two floats.
+"""
+def add(a: float, b: float) -> float:
+    """Adds two floats and return the result."""
+    return a + b
